a91b0faf775e85037d811cd28d8df23e37397779
Dmytro Krasylnikov <krasylnikov@gmail.com>
2013-02-25 13:11:24 +0200

    Initial Commit

---
+++ KarapuzTests/KarapuzTests.m
@@ -0,0 +1,32 @@
+//
+//  KarapuzTests.m
+//  KarapuzTests
+//
+//  Created by krasylnikov on 2/25/13.
+//  Copyright (c) 2013 Design and Test lab. All rights reserved.
+//
+
+#import "KarapuzTests.h"
+
+@implementation KarapuzTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KarapuzTests");
+}
+
+@end
