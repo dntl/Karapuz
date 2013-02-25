d06ab25e31a83228fccbf307a2b04047936c6921
Dmytro Krasylnikov <krasylnikov@gmail.com>
2013-02-25 13:11:24 +0200

    Initial Commit

---
+++ Karapuz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Karapuz
+//
+//  Created by krasylnikov on 2/25/13.
+//  Copyright (c) 2013 Design and Test lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KarapuzTests/KarapuzTests.h
@@ -0,0 +1,13 @@
+//
+//  KarapuzTests.h
+//  KarapuzTests
+//
+//  Created by krasylnikov on 2/25/13.
+//  Copyright (c) 2013 Design and Test lab. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KarapuzTests : SenTestCase
+
+@end
