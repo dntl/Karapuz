605b341c6ab31556a91a2230083bb9fd3dc758d6
Dmytro Krasylnikov <krasylnikov@gmail.com>
2013-03-06 16:47:38 +0200

    Initial Commit

---
+++ krpzosx/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  krpzosx
+//
+//  Created by krasylnikov on 3/6/13.
+//  Copyright (c) 2013 Design and Test lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
